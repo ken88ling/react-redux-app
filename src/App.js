5ad9acbce4274cc865e5f80d8dc2cf2baf682f48
Ken Ling <ken88ling@gmail.com>
2018-10-11 23:35:08 +0800

    add post

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./components/Posts";
+import Postform from "./components/Postform";
 
 class App extends Component {
   render() {
@@ -20,6 +22,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Postform />
+        <hr />
+        <Posts />
       </div>
     );
   }
